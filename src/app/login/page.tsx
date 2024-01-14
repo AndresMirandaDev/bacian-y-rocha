da39245a1c4ec3c1dea9fb123535c1170995433a
Andres Miranda <andres.miranda.dev@gmail.com>
2024-01-14 22:27:58 +0900

    convert login page to server component and remove useeffect

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,4 +1,3 @@
-'use client';
 import { Box, Button, Card, Flex, Text } from '@radix-ui/themes';
 import { signIn } from 'next-auth/react';
 import Image from 'next/image';
@@ -8,22 +7,22 @@ import { RiLoginCircleLine } from 'react-icons/ri';
 import LoadingScreen from '../components/LoadingScreen';
 
 const LoginPage = () => {
-  const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    const checkIfInitialLoad = !localStorage.getItem('isInitialLoad');
+  //   const [loading, setLoading] = useState(true);
+  //   useEffect(() => {
+  //     const checkIfInitialLoad = !localStorage.getItem('isInitialLoad');
 
-    if (checkIfInitialLoad) {
-      // It's the initial load (refreshing the page)
-      localStorage.setItem('isInitialLoad', 'true');
-      setLoading(true); // Set loading to true when refreshing
-    } else {
-      // It's not the initial load
-      localStorage.removeItem('isInitialLoad');
-      setLoading(false); // Set loading to false after rendering
-    }
-  }, []);
+  //     if (checkIfInitialLoad) {
+  //       // It's the initial load (refreshing the page)
+  //       localStorage.setItem('isInitialLoad', 'true');
+  //       setLoading(true); // Set loading to true when refreshing
+  //     } else {
+  //       // It's not the initial load
+  //       localStorage.removeItem('isInitialLoad');
+  //       setLoading(false); // Set loading to false after rendering
+  //     }
+  //   }, []);
 
-  if (loading) return <LoadingScreen />;
+  //   if (loading) return <LoadingScreen />;
   return (
     <div className="flex h-screen w-screen  justify-center items-center bg-cyan-700">
       <Flex
