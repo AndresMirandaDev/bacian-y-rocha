946b338fa92878c9402cb13bb3615a2b71aea2d1
Andres Miranda <andres.miranda.dev@gmail.com>
2024-02-05 13:45:43 +0900

    comment out unused assignedmaterials api

--- src/app/api/assignedmaterials/route.ts
+++ src/app/api/assignedmaterials/route.ts
@@ -1,16 +1,16 @@
-import { NextRequest, NextResponse } from "next/server";
-import prisma from "../../../../prisma/client";
+// import { NextRequest, NextResponse } from "next/server";
+// import prisma from "../../../../prisma/client";
 
-export async function GET(request:NextRequest) {
-    try {
-        const assignedMaterials = await prisma.assignedMaterials.findMany({
-            include:{
-                saleOrder:true,
-                workOrder:true
-            }
-        })
-        return NextResponse.json({ message: 'success', body: assignedMaterials })
-    } catch (error) {
-        return NextResponse.json({ message:'An unexpected error occurred' }, { status: 500 })
-    }
-}
+// export async function GET(request:NextRequest) {
+//     try {
+//         const assignedMaterials = await prisma.assignedMaterials.findMany({
+//             include:{
+//                 saleOrder:true,
+//                 workOrder:true
+//             }
+//         })
+//         return NextResponse.json({ message: 'success', body: assignedMaterials })
+//     } catch (error) {
+//         return NextResponse.json({ message:'An unexpected error occurred' }, { status: 500 })
+//     }
+// }
