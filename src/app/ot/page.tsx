36c369450ae880b03d66e68d9c52a0d2e3995b73
Andres Miranda <andres.miranda.dev@gmail.com>
2024-02-08 15:19:46 +0900

    add new workorder button to ot page

--- src/app/ot/page.tsx
+++ src/app/ot/page.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
-import { Card, Grid } from '@radix-ui/themes';
+import { Box, Button, Card, Grid } from '@radix-ui/themes';
 import prisma from '../../../prisma/client';
 import { notFound } from 'next/navigation';
 import WorkOrdersTable from './_otTables/WorkOrdersTable';
+import Link from 'next/link';
+import { PlusIcon } from '@radix-ui/react-icons';
 
 const WorkOrderPage = async () => {
   const workOrders = await prisma.workOrder.findMany();
 
   if (!workOrders) return notFound();
   return (
-    <Grid className="p-3">
+    <Grid className="p-3" gap="4">
+      <Box>
+        <Button>
+          <PlusIcon />
+          <Link href={'/ot/new'}>Registrar orden de trabajo</Link>
+        </Button>
+      </Box>
       <Card>
         <WorkOrdersTable workOrders={workOrders} />
       </Card>
