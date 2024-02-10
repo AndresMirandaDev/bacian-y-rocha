462076f8996979a9169691ba233d1fdd4d9370a0
Andres Miranda <andres.miranda.dev@gmail.com>
2024-02-10 11:40:54 +0900

    change argument to page

--- src/app/quotes/QuoteForm.tsx
+++ src/app/quotes/QuoteForm.tsx
@@ -104,7 +104,7 @@ const QuoteForm = ({ quote }: Props) => {
 
         await axios.patch(`/api/quotes/${quote.id}`, updatedData);
 
-        revalidatePath('/quotes', 'layout');
+        revalidatePath('/quotes', 'page');
         router.push('/quotes');
         router.refresh();
         toast.success('Cotización ha sido actualizada.');
