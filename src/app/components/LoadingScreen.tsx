5cf61ca625302be1f535700b10a5b6480214ed6d
Andres Miranda <andres.miranda.dev@gmail.com>
2024-02-20 10:41:39 +0900

    fix loading screen

--- src/app/components/LoadingScreen.tsx
+++ src/app/components/LoadingScreen.tsx
@@ -4,7 +4,7 @@ import logo from '../../../public/assets/images/byrs.png';
 
 const LoadingScreen = () => {
   return (
-    <div className="flex justify-center items-center h-screen w-screen absolute ">
+    <div className="flex justify-center items-center h-[calc(100vh-3rem)]">
       <Image src={logo} alt="logo" height={200} width={200} />
       <span className="loading loading-ring loading-lg block"></span>
     </div>
