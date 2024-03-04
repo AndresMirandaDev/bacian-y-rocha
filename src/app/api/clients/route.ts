e13a05bffa79517bff9343cc23ec4df3ae189f81
Andres Miranda <andres.miranda.dev@gmail.com>
2024-03-04 11:57:25 +0900

    create stakeholders container

---
+++ src/app/api/clients/route.ts
@@ -0,0 +1,49 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "../../../../prisma/client";
+import { stakeholderValidationSchema } from "./validationSchema";
+import { StakeholderType } from "@prisma/client";
+
+export async function GET(request:NextRequest) {
+    try {
+        const clients = await prisma.stakeholders.findMany({
+            where: {type : StakeholderType.CLIENT}
+        })
+        return NextResponse.json({message:'success', body: clients})
+    } catch (error) {
+        console.log(error)
+        return NextResponse.json({ message:'An unexpected error occurred',error }, { status:500 })
+    }
+}
+
+export async function POST (request:NextRequest) {
+    const body = await request.json()
+    const isValid = await stakeholderValidationSchema.safeParse(body)
+    try {
+        if(!isValid.success){
+            return NextResponse.json({message:isValid.error.format()}, {status:400})
+        }
+        const { name,
+                email,
+                city,
+                phone,
+                rut,
+                type} = body
+
+        const newClient = await prisma.stakeholders.create({
+            data:{
+                name,
+                email,
+                city,
+                phone,
+                rut,
+                type
+            }
+        })
+
+        return NextResponse.json({message:'success', body: newClient})
+
+    } catch (error) {
+        console.log(error)
+        return NextResponse.json({ message:'An unexpected error occurred',error }, { status:500 })
+    }
+}
---
+++ src/app/api/clients/validationSchema.ts
@@ -0,0 +1,17 @@
+import z from 'zod'
+
+export const stakeholderValidationSchema = z.object({
+    name: z.string().min(1).max(255),
+    email: z.string().min(1).max(255).email(),
+    phone: z.string().min(1).max(255),
+    city: z.string().min(1).max(255),
+    rut: z.string().min(1).max(255),
+    type: z.string().min(1).max(255),
+})
+export const updateStakeholderValidationSchema = z.object({
+    name: z.string().min(1).max(255).optional(),
+    email: z.string().min(1).max(255).email().optional(),
+    phone: z.string().min(1).max(255).optional(),
+    city: z.string().min(1).max(255).optional(),
+    rut: z.string().min(1).max(255).optional(),
+})
